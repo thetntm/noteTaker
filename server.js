52f7a5f752d9eb1a0bb76872f03981a116ffa8b9
thetntm <michael.becker.123@gmail.com>
2020-03-23 21:17:31 -0700

    fixed everything it's all good now

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const fs = require("fs");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const publicDirectory = path.join(__dirname, "public");
 
